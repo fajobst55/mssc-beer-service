85348a418432d44a90420f7ca08be7a517861d2e
Frank Jobst <fajobst@gmail.com>
2021-05-11 19:32:54 -0500

    updated UPC. closes #14

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -40,11 +40,9 @@ public class BeerDto {
     @NotNull
     private BeerStyleEnum beerStyle;
 
-    @Positive
     @NotNull
-    private Long upc;
+    private String upc;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
