34d2fcacefff093fa4901287d309aaf00b087017
Frank Jobst <fajobst@gmail.com>
2021-05-23 08:51:37 -0500

    configure-jms closes #25

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-21T15:56:29-0500",
+    date = "2021-05-23T08:45:03-0500",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.9 (Oracle Corporation)"
 )
 @Component
