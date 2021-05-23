0b2c712fd016432d537ab663a35aedcb936e96d4
Frank Jobst <fajobst@gmail.com>
2021-05-23 14:07:51 -0500

    fix-jackson-configuration. closes #30

--- src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
+++ src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
@@ -2,19 +2,18 @@ package guru.springframework.msscbeerservice.events;
 
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
-import lombok.Builder;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Data
-@RequiredArgsConstructor
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BeerEvent implements Serializable {
 
     static final long serialVersionUID = 7534116684480527094L;
 
-    private final BeerDto beerDto;
+    private BeerDto beerDto;
 
 }
--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-23T08:45:03-0500",
+    date = "2021-05-23T13:58:47-0500",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.9 (Oracle Corporation)"
 )
 @Component
